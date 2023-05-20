d0be5452635915b5d82262884b09fb9d458650d1
Onovo-fortune20 <onovojr@gmail.com>
2023-05-20 18:07:06 +0000

    new

--- adminpage.js
+++ adminpage.js
@@ -2,6 +2,8 @@
 const resetPasswordButton = document.querySelector('.edit-button');
 const saveButton = document.querySelector('.save-button');
 const passwordInput = document.querySelector('input[type="password"]');
+const deleteButton = document.querySelector('.block-button');
+
 
 // Add event listener to the button
 resetPasswordButton.addEventListener('click', function () {
@@ -21,6 +23,17 @@ saveButton.addEventListener('click', function () {
     confirmButtonText: 'OK'
   });
 });
+deleteButton.addEventListener('click', function () {
+  // Get the parent row element
+  const row = deleteButton.parentNode.parentNode;
+
+  // Remove the row from the table
+  row.parentNode.removeChild(row);
+});
+
+function red() {
+  window.location.href = "index.html";
+}
 
 function red() {
   window.location.href ="index.html";
--- sign.js
+++ sign.js
@@ -1,76 +1,81 @@
-let signupBtn = document.getElementById("signupBtn")
-let signinBtn = document.getElementById("signinBtn")
-let nameField = document.getElementById("nameField")
-let title = document.getElementById("title")
-let number = document.getElementById("numberField")
+let signupBtn = document.getElementById("signupBtn");
+let signinBtn = document.getElementById("signinBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+let number = document.getElementById("numberField");
 
-signinBtn.onclick= function(){
-    nameField.style.maxHeight = "0"
-    title.innerHTML = "Sign In";
-    signupBtn.classList.add("disable")
-    signinBtn.classList.remove("disable")
-}
-function redirect() {
-    window.location.href = "index.html";
+signinBtn.onclick = function () {
+  nameField.style.maxHeight = "0";
+  title.innerHTML = "Sign In";
+  signupBtn.classList.add("disable");
+  signinBtn.classList.remove("disable");
+};
+function redirect(event) {
+  event.preventDefault();
+  // window.location.href = "index.html";
+  const username = "onovojr@gmail.com";
+  const password = "Fortunes19E_";
+  if (
+    username === document.getElementById("email").value &&
+    password === document.getElementById("password").value
+  ) {
+    window.location.assign("index.html");
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Wrong User data ",
+      confirmButtonText: "OK",
+    });
   }
-  
-signupBtn.onclick= function(){
-    nameField.style.maxHeight = "60px"
-    title.innerHTML = "Sign Up";
-    signupBtn.classList.remove("disable")
-    signinBtn.classList.add("disable")
 }
+
+signupBtn.onclick = function () {
+  nameField.style.maxHeight = "60px";
+  title.innerHTML = "Sign Up";
+  signupBtn.classList.remove("disable");
+  signinBtn.classList.add("disable");
+};
 let timeout;
 
 // traversing the DOM and getting the input and span using their IDs
 
-let password = document.getElementById('password')
-let passwordconfirm = document.getElementById('conpassword')
-let strengthBadge = document.getElementById('strength')
-let passwordstatement = document.getElementById("passwordchecker")
+let password = document.getElementById("password");
+let passwordconfirm = document.getElementById("conpassword");
+let strengthBadge = document.getElementById("strength");
+let passwordstatement = document.getElementById("passwordchecker");
 
 // The strong and weak password Regex pattern checker
 
-let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
-let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,}))')
+let strongPassword = new RegExp(
+  "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})"
+);
+let mediumPassword = new RegExp("((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,}))");
 
 function StrengthChecker(PasswordParameter) {
-    // We then change the badge's color and text based on the password strength
+  // We then change the badge's color and text based on the password strength
 
-    if (strongPassword.test(PasswordParameter)) {
-        strengthBadge.style.color = "green"
-        strengthBadge.textContent = 'Strong'
-    }
-    else if (PasswordParameter <= 0) {
-        strengthBadge.textContent = ''
-    }
-    else if (mediumPassword.test(PasswordParameter)) {
-        strengthBadge.style.color = 'blue'
-        strengthBadge.textContent = 'Medium'
-    } else {
-        strengthBadge.style.color = 'red'
-        strengthBadge.textContent = 'Weak'
-    }
+  if (strongPassword.test(PasswordParameter)) {
+    strengthBadge.style.color = "green";
+    strengthBadge.textContent = "Strong";
+  } else if (PasswordParameter <= 0) {
+    strengthBadge.textContent = "";
+  } else if (mediumPassword.test(PasswordParameter)) {
+    strengthBadge.style.color = "blue";
+    strengthBadge.textContent = "Medium";
+  } else {
+    strengthBadge.style.color = "red";
+    strengthBadge.textContent = "Weak";
+  }
 }
 
-// Adding an input event listener when a user types to the  password input 
+// Adding an input event listener when a user types to the  password input
 
 password.addEventListener("input", () => {
+  // strengthBadge.style.display= 'block'
+  clearTimeout(timeout);
 
+  //We then call the StrengChecker function as a callback then pass the typed password to it
 
-    // strengthBadge.style.display= 'block'
-    clearTimeout(timeout);
-
-    //We then call the StrengChecker function as a callback then pass the typed password to it
-
-    timeout = setTimeout(() => StrengthChecker(password.value), 500);
-    
-
+  timeout = setTimeout(() => StrengthChecker(password.value), 500);
 });
-
-
-
-
-
-
-

5bb38aa8a1c01eba42aed029d09fe228fd44d6a1
Onovo-fortune20 <onovojr@gmail.com>
2023-05-20 01:37:55 +0000

    newest

---
+++ adminpage.js
@@ -0,0 +1,23 @@
+
+const resetPasswordButton = document.querySelector('.edit-button');
+const saveButton = document.querySelector('.save-button');
+const passwordInput = document.querySelector('input[type="password"]');
+
+// Add event listener to the button
+resetPasswordButton.addEventListener('click', function () {
+  // Toggle the disabled attribute of the password input field
+  passwordInput.disabled = !passwordInput.disabled;
+});
+
+saveButton.addEventListener('click', function () {
+  // Disable the password input field
+  passwordInput.disabled = true;
+
+  // Display a success message
+  Swal.fire({
+    icon: 'success',
+    title: 'Success',
+    text: 'User data updated',
+    confirmButtonText: 'OK'
+  });
+});
